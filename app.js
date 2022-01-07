277c4e84b93cb701730bef8715f425334b5181b4
UIT20521569 <20521569@gm.uit.edu.vn>
2022-01-08 00:43:57 +0700

    Update app.js

--- app.js
+++ app.js
@@ -13,10 +13,10 @@
 11.
  **Lưu ý sửa một số bug trong app cần fix sau khi thi xong :D
 1. Tìm hiểu về lỗi GET http://127.0.0.1:5500/favicon.ico 404 (Not Found)
-mỗi khi vào chạy app ở mục console và fix.
-2. Tua nhạc thường bị lỗi phải bấm nhiều lần mới được
+mỗi khi vào chạy app ở mục console và fix. ==> Done
+2. Tua nhạc thường bị lỗi phải bấm nhiều lần mới được  ==> Done
 3. Nút Repeat và nút Shuffle cùng active
-4. Shuffle chưa tối ưu, 
+4. Shuffle chưa tối ưu
 nên đưa tất cả bài nhạc vào một mảng
 mỗi khi nó chạy thì lại remove nó ra cho đến
 khi mảng về rỗng thì reset quá trình
@@ -68,52 +68,52 @@ const app = {
             image: "https://e.dowload.vn/data/image/2021/03/20/loi-bai-hat-giua-dai-lo-dong-tay-700.jpg"
         },
         {
-            name: "Muộn Rồi Mà Sao Còn",
-            singer: "Sơn Tùng M-TP",
-            path: "https://c1-ex-swe.nixcdn.com/Believe_Audio19/MuonRoiMaSaoCon-SonTungMTP-7011803.mp3?st=tD-Ln6qGqkdH659AeuHsjQ&e=1638782546",
-            image: "https://avatar-nct.nixcdn.com/song/2021/04/29/9/1/f/8/1619691182261.jpg"
+            name: "Tháng Mấy Anh Nhớ Em",
+            singer: "Hà Anh Tuấn",
+            path: "https://tainhac365.org/download-music/459824",
+            image: "https://i.ytimg.com/vi/lxFmeBhoA1Y/maxresdefault.jpg"
         },
         {
-            name: "Thức Giấc",
+            name: "Thanh Xuân",
             singer: "Da LAB",
-            path: "https://c1-ex-swe.nixcdn.com/NhacCuaTui1018/ThucGiac-DaLAB-7048212.mp3?st=1LcQhTisk8WrOQuzK4p86Q&e=1638782708",
-            image: "https://avatar-nct.nixcdn.com/song/2021/07/14/8/c/f/9/1626231010810.jpg"
+            path: "https://tainhac365.org/download-music/25726",
+            image: "https://avatar-nct.nixcdn.com/song/2018/08/24/b/a/9/5/1535121377317_640.jpg"
         },
         {
-            name: "Độ Tộc 2",
-            singer: "Masew, Độ Mixi, Phúc Du, Pháo",
-            path: "https://c1-ex-swe.nixcdn.com/NhacCuaTui1020/DoToc2-MasewDoMixiPhucDuPhao-7064730.mp3?st=ehahZN3-iX9xYdBFgDgGcg&e=1638782785",
-            image: "https://avatar-nct.nixcdn.com/song/2021/08/10/b/2/e/0/1628579601228.jpg"
+            name: "Nơi Ấy Bình Yên",
+            singer: "Hà Anh Tuấn",
+            path: "https://tainhac365.org/download-music/25725",
+            image: "https://avatar-nct.nixcdn.com/song/2017/07/18/0/e/e/2/1500362680062_640.jpg"
         },
         {
-            name: "Chúng Ta Sau Này",
-            singer: "T.R.I",
-            path: "https://c1-ex-swe.nixcdn.com/NhacCuaTui1010/ChungTaSauNay-TRI-6929586.mp3?st=l56Wr1fLE9fMnFehhpo5xg&e=1638782875",
-            image: "https://avatar-nct.nixcdn.com/song/2021/01/27/5/2/2/b/1611738358661.jpg"
+            name: "Xin Lỗi",
+            singer: "Hồ Tiến Đạt",
+            path: "https://tainhac365.org/download-music/197707",
+            image: "https://avatar-nct.nixcdn.com/song/2017/12/13/6/6/b/e/1513149192270_640.jpg"
         },
         {
-            name: "Dịu Dàng Em Đến",
-            singer: "ERIK, NinjaZ",
-            path: "https://c1-ex-swe.nixcdn.com/NhacCuaTui1021/DiuDangEmDen-ERIKNinjaZ-7078877.mp3?st=QmjyqbnGv3jClPKm4oA1YQ&e=1638782938",
-            image: "https://avatar-nct.nixcdn.com/song/2021/08/30/2/1/a/e/1630307726211.jpg"
+            name: "Chưa Bao Giờ",
+            singer: "Hà Anh Tuấn",
+            path: "https://tainhac365.org/download-music/17078",
+            image: "https://avatar-nct.nixcdn.com/song/2017/08/08/0/6/6/b/1502178209742_640.jpg"
         },
         {
-            name: "Hương",
-            singer: "Văn Mai Hương, Negav",
-            path: "https://c1-ex-swe.nixcdn.com/NhacCuaTui1010/Huong-VanMaiHuongNegav-6927340.mp3?st=PvHOWlRnF6TymvggYGding&e=1638783027",
-            image: "https://avatar-nct.nixcdn.com/song/2021/01/22/9/f/2/1/1611280898757.jpg"
+            name: "Cầu Hôn",
+            singer: "Văn Mai Hương",
+            path: "https://tainhac365.org/download-music/100209",
+            image: "https://avatar-nct.nixcdn.com/song/share/2019/01/18/6/4/b/7/1547782985971.jpg"
         },
         {
-            name: "Miên Man",
-            singer: "DUTZUX",
-            path: "https://c1-ex-swe.nixcdn.com/NhacCuaTui1024/MienMan-DUTZUX-7120884.mp3?st=yTOFq5aH8FGEvbm-_n_KTA&e=1638783090",
-            image: "https://avatar-nct.nixcdn.com/song/2021/11/19/6/d/9/1/1637320885751.jpg"
+            name: "Chuyện Rằng",
+            singer: "Thịnh Suy",
+            path: "https://tainhac365.org/download-music/391188",
+            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwwYU_vimhKlY3IfxF6huv2a37bhiQ8Bd57w&usqp=CAU"
         },
         {
-            name: "có hẹn với thanh xuân",
-            singer: "MONSTAR",
-            path: "https://c1-ex-swe.nixcdn.com/NhacCuaTui1020/cohenvoithanhxuan-MONSTAR-7050201.mp3?st=PjrrnZ2dZ3ffA6R7dRrppQ&e=1638783161",
-            image: "https://avatar-nct.nixcdn.com/song/2021/07/16/f/4/9/8/1626425507034.jpg"
+            name: "Mùa Yêu Đầu",
+            singer: "Đinh Mạnh Ninh",
+            path: "https://tainhac365.org/download-music/12431",
+            image: "https://avatar-nct.nixcdn.com/singer/avatar/2016/01/25/4/1/1/7/1453715990328_600.jpg"
         }
     ],
 
@@ -203,9 +203,16 @@ const app = {
         }
 
         //Xử lý khi tua bài hát 
+        progress.oninput = function (e){
+            const seekTime = e.target.value * audio.duration / 100
+            audio.currentTime = seekTime
+            audio.pause()
+        }
+
         progress.onchange = function(e){
             const seekTime = e.target.value * audio.duration / 100
             audio.currentTime = seekTime
+            audio.play()
         }
         
 
@@ -347,20 +354,3 @@ const app = {
 
 app.start()
 
-/* **Lưu ý sửa một số bug trong app cần fix sau khi thi xong :D
-1. Tìm hiểu về lỗi GET http://127.0.0.1:5500/favicon.ico 404 (Not Found)
-mỗi khi vào chạy app ở mục console và fix.
-2. Tua nhạc thường bị lỗi phải bấm nhiều lần mới được
-3. Nút Repeat và nút Shuffle cùng active
-4. Shuffle chưa tối ưu, 
-nên đưa tất cả bài nhạc vào một mảng
-mỗi khi nó chạy thì lại remove nó ra cho đến
-khi mảng về rỗng thì reset quá trình
-5. Nên Css thêm cho prev và next Btn, sao cho khi bấm cảm giác được 
-rằng mình đã bấm vào
-6. Css Hover cho những nút
-7. Mỗi lần active cho các bài nhạc khác thường phải render lại
-8. Phần active nhạc thì sẽ scroll đến bài đó nhưng lại chưa tối ưu
-9. Xử lý click option những bài khác sẽ không nhận(dòng 253)
-9. Thêm các tính năng vào option 3 chấm 
-*/
